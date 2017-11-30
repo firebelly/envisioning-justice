f3971b0084da2bb67d38ee3fa617fad2f3f81a30
poopsplat <matt.m.soria@gmail.com>
2017-11-30 15:44:16 -0800

    Styling the newsletter form in the footer, adding a `theme-exception` class for buttons

--- web/app/themes/envisioningjustice/templates/footer.php
+++ web/app/themes/envisioningjustice/templates/footer.php
@@ -5,15 +5,19 @@
 
         <form id="newsletter" class="newsletter-form">
           <h3 class="type-h3">Subscribe to our Newsletter</h3>
-          <div class="input-wrap">
-            <label for="newsletterName">Full Name</label>
-            <input type="text" id="newsletterName" name="newsletterName">
+          <div class="-bottom">
+            <div class="form-group">          
+              <div class="input-wrap">
+                <label for="newsletterName">Full Name</label>
+                <input type="text" id="newsletterName" name="newsletterName">
+              </div>
+              <div class="input-wrap">
+                <label for="newsletterEmail">Email Address</label>
+                <input type="email" id="newsletterEmail" name="newsletterEmail">
+              </div>
+            </div>
+            <button type="submit" class="button button-circular submit -white-dark theme-exception"><span class="sr-only">Submit</span><svg class="icon icon-arrow" aria-hidden="true" role="image"><use xlink:href="#icon-arrow"/></svg></button>
           </div>
-          <div class="input-wrap">
-            <label for="newsletterEmail">Email Address</label>
-            <input type="email" id="newsletterEmail" name="newsletterEmail">
-          </div>
-          <button type="submit" class="button submit"><span class="sr-only">Submit</span></button>
         </form>
 
         <div class="logos">
