7e8fb1ff9bdc00522d01295316a3f34e1eae911a
poopsplat <matt.m.soria@gmail.com>
2017-12-05 10:06:37 -0800

    Fixing some responsive styling for the site footer

--- web/app/themes/envisioningjustice/templates/footer.php
+++ web/app/themes/envisioningjustice/templates/footer.php
@@ -1,7 +1,15 @@
 <footer class="site-footer" role="contentinfo">
-  <div class="container">
-    <div class="grid">
-      <div class="md-one-half">
+
+  <div class="slashfield-container container">
+    <div class="-inner">
+      <div class="slashfield" data-rows="3"></div>
+    </div>
+  </div>
+
+  <div class="content-container container">
+    <div class="grid md-col-spaced">
+
+      <div class="grid-item md-one-half -first">
 
         <form id="newsletter" class="newsletter-form">
           <h3 class="type-h3">Subscribe to our Newsletter</h3>
@@ -21,14 +29,30 @@
         </form>
 
         <div class="logos">
-          <a href="http://ilhumanities.org"><span class="sr-only">IllinoisHumanities</span><svg class="il-humanities-logo" aria-hidden="true" role="presentation"><use xlink:href="#ih-logo-color"/></svg></a>
+          <a href="http://ilhumanities.org"><span class="sr-only">Illinois Humanities</span><svg class="il-humanities-logo" aria-hidden="true" role="presentation"><use xlink:href="#ih-logo-color"/></svg></a>
           <a href="/"><span class="sr-only"><?php bloginfo('name'); ?></span><svg class="icon envisioning-justice-logo" aria-hidden="true" role="presentation"><use xlink:href="#envisioning-justice-logo"/></svg></a>
         </div>
-        <p class="disclaimer">Envisioning Justice is a program of the Illinoise Humanities Council any additional copy vivamus non tortor laoreet, fermentum mi vitae, placerat tortor. Maecenas.</p>
+        <p class="site-disclaimer"><?= \Firebelly\SiteOptions\get_option('disclaimer'); ?></p>
+
       </div>
-      <div class="md-one-half">
+
+      <div class="site-contact grid-item md-one-half -second">
+
         <h3 class="type-h3">Contact</h3>
+
+        <div class="site-communication -item">
+          <p><a href="mailto:<?= \Firebelly\SiteOptions\get_option('contact_email'); ?>"><?= \Firebelly\SiteOptions\get_option('contact_email'); ?></a></p>
+          <p><?= \Firebelly\SiteOptions\get_option('contact_phone'); ?></p>
+        </div>
+        <div class="site-address -item">        
+          <address class="vcard">
+            <span class="street-address"><?= \Firebelly\SiteOptions\get_option('contact_address'); ?></span>
+            <span class="locality"><?= \Firebelly\SiteOptions\get_option('contact_locality'); ?></span>
+          </address>
+        </div>
+
       </div>
+
     </div>
   </div>
 </footer>
