473bbfcd0c79ba2f2270840ee3d502d087bd4f8e
poopsplat <matt.m.soria@gmail.com>
2017-12-20 06:34:05 -0800

    Changing the server location for the staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,7 +9,7 @@ set :application, 'envisioningjustice_staging'
 
 # Extended Server Syntax
 # ======================
-server 'ilhumanities.webfactional.com', user: fetch(:login), roles: %w{web app db}
+server 'staging.envisioningjustice.org', user: fetch(:login), roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
