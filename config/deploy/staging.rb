452ca3de5281c370e3f3e7d2d92c7859d1c6ef4d
poopsplat <matt.m.soria@gmail.com>
2017-12-20 09:09:30 -0800

    Switching staging location to firebelly.co server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 set :stage, :staging
-set :application, 'envisioningjustice_staging'
+set :login, 'firebelly'
+set :application, 'envisioningjustice'
 
 # Simple Role Syntax
 # ==================
@@ -9,7 +10,7 @@ set :application, 'envisioningjustice_staging'
 
 # Extended Server Syntax
 # ======================
-server 'staging.envisioningjustice.org', user: fetch(:login), roles: %w{web app db}
+server 'envisioningjustice.firebelly.co', user: fetch(:login), roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
