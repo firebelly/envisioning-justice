ffc71b516907d3258982b77fe93aebbd1850f452
poopsplat <matt.m.soria@gmail.com>
2018-02-20 10:13:50 -0800

    Fixing search form

--- web/app/themes/envisioningjustice/templates/searchform.php
+++ web/app/themes/envisioningjustice/templates/searchform.php
@@ -2,8 +2,8 @@
   <div class="-inner">  
     <h3>Search</h3>
     <div class="input-wrap">    
-      <label for="search">Keyword(s)</label>
-      <input id="search" type="search" value="" autocomplete="off" name="search" class="search-field form-control" placeholder="" required>
+      <label for="s">Keyword(s)</label>
+      <input id="s" type="search" value="" autocomplete="off" name="s" class="search-field form-control" placeholder="" required>
     </div>
     <button type="submit" class="search-submit button">Go</button>
     <div class="search-close"><span class="text">Close Search</span> <svg class="icon icon-plus" aria-hidden="true" role="presentation"><use xlink:href="#icon-plus"/></svg></div>
