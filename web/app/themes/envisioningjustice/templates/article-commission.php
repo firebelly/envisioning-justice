d0a008faac48c2650b139c4366135e4c4cb4f4c4
Nate Beaty <nate@clixel.com>
2019-07-02 14:59:20 -0400

    share styles between commissions and projects

--- web/app/themes/envisioningjustice/templates/article-commission.php
+++ web/app/themes/envisioningjustice/templates/article-commission.php
@@ -3,8 +3,8 @@
   $header_bg = \Firebelly\Media\get_header_bg($commission_post);
 ?>
 <li class="commission grid-item sm-one-half big-clicky">
-  <div class="commission-image-wrap">
-    <div class="commission-image" <?= $header_bg ?>></div>
+  <div class="image-wrap">
+    <div class="image" <?= $header_bg ?>></div>
   </div>
   <h2 class="type-h2"><a href="<?= $commission_url ?>"><?= $commission_post->post_title ?></a></h2>
 </li>
